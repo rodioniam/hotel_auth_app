cbb9b8c4f769cf22b5b4f58e207f0200a025bdb2
Rodion Iamshanov <roiamshanov@gmail.com>
2026-04-27 17:29:13 +0200

    add password length validation and email uniqueness check

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,17 +4,28 @@
 
 
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import User
 
 
 class UserRegisterSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all(),
+                            message='Account with such email already exists!')
+        ]
+    )
     password = serializers.CharField()
     password_confirm = serializers.CharField()
     first_name = serializers.CharField()
     middle_name = serializers.CharField(
         required=False, allow_null=True, allow_blank=True)
     last_name = serializers.CharField()
+
+    def validate_password(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError('Password must contain at least 10 characters')  # noqa
+        return value
 
     def validate(self, data):
         if data['password'] != data['password_confirm']:
@@ -55,6 +66,11 @@ class PasswordChangeSerializer(serializers.Serializer):
     old_password = serializers.CharField()
     new_password = serializers.CharField()
     new_password_confirm = serializers.CharField()
+
+    def validate_new_password(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError('Password must contain at least 10 characters')  # noqa
+        return value
 
     def validate(self, data):
         if data['new_password'] != data['new_password_confirm']:
